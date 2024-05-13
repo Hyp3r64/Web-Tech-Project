c666c1614b5d51e57ba4e5038205d8ef6caa82a1
Hossain Al Arik <110621058+Hyp3r64@users.noreply.github.com>
2024-05-14 04:55:17 +0600

    Update product-page.php

--- GameStop/Views/product-page.php
+++ GameStop/Views/product-page.php
@@ -4,9 +4,6 @@
     require_once('../Models/review-info-model.php'); 
     require_once('../Controllers/message-controller.php'); 
 
-    if(!isset($_COOKIE['flag'])){
-        popup("Error!","You need to sign-in in order to access this page.");
-    }
     $id = $_COOKIE['id'];
     $row = UserInfo($id);
     $pid = $_GET['pid'];
